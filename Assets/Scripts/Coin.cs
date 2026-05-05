0039ce9dcb7f61b2de1789b7c1e6db0953df8ada
Chen <xunchenliang@gmail.com>
2026-05-05 22:31:05 +0800

    0505
    金幣隨機生成

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -5,21 +5,31 @@ public class Coin : MonoBehaviour
     [SerializeField] private int coinValue = 10;
     [SerializeField] private GameObject moneyPopupPrefab; // 稍後製作的特效預製物
 
+    void Update()
+    {
+        transform.Rotate(0f, 0f, 100f * Time.deltaTime);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         // 檢查撞到的是不是玩家（記得要把 Capsule 的 Tag 設為 "Player"）
         if (other.CompareTag("Player"))
         {
-            // 1. 給玩家錢
+            
             other.GetComponent<PlayerStats>().AddMoney(coinValue);
 
-            // 2. 在金幣位置生成「賺錢文字」特效
+            // 賺錢文字特效
             if (moneyPopupPrefab != null)
             {
                 Instantiate(moneyPopupPrefab, transform.position + Vector3.up, Quaternion.identity);
             }
 
-            // 3. 金幣消失
+            CoinSpawner spawner = FindObjectOfType<CoinSpawner>();
+            if (spawner != null)
+            {
+                spawner.OnCoinCollected();
+            }
+            //金幣消失
             Destroy(gameObject);
         }
     }
